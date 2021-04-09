37d8df2c8e9705f66db4547c993c0a341e3aa8a0
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-09 11:14:43 +0900

    Update install and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      white: colors.white,
+    },
   },
   variants: {
     extend: {},
